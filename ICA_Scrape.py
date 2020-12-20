897e23cba034220d480ad0b312f1cca2933b354e
Nikhil Gohil <nikhil.n.gohil@gmail.com>
2020-12-20 15:33:32 -0800

    Initial beautifulsoup coding

--- ICA_Scrape.py
+++ ICA_Scrape.py
@@ -1,1 +1,17 @@
-##Script to scrape apartment cost from Irvine Company apartment
+##Script to scrape apartment cost from Irvine Company apartments website
+from bs4 import BeautifulSoup
+import requests
+import csv
+import sys
+import pprint
+
+printer  = pprint.PrettyPrinter(indent=4)
+soup_output = open("source_soup.txt", "w")
+
+
+source = requests.get('https://www.irvinecompanyapartments.com/locations/orange-county/irvine/spectrum/village-at-irvine-spectrum/availability.html#').text
+
+soup = BeautifulSoup(source, 'lxml')
+#printer.pprint(soup)
+print(source, file=soup_output)
+print("Hello World!")
