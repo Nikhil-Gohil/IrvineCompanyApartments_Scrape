7e5b2ffb63233597966c6232541a3c463d9d7ca5
Nikhil Gohil <nikhil.n.gohil@gmail.com>
2020-12-21 01:46:22 -0800

    QOL Improvements, efficiency and beautification

--- ICA_Scrape.py
+++ ICA_Scrape.py
@@ -15,9 +15,7 @@ printer = pprint.PrettyPrinter(indent=4)
 
 def main():
     wb = open_excel_workbook()
-
     outfile = build_outfile_name()
-
     url_dict = create_populate_url_dict()
 
     for name, url in url_dict.items():
@@ -28,6 +26,9 @@ def main():
 
 
 def open_excel_workbook():
+    """
+    Create an openpyxl workbook, delete default sheet and return it.
+    """
     wb = openpyxl.Workbook()
     del wb["Sheet"]
     # wb.remove_s(wb.get_sheet_by_name("Sheet"))
@@ -35,6 +36,9 @@ def open_excel_workbook():
 
 
 def build_outfile_name():
+    """
+    Builds output filename based on current date and time.
+    """
     now = datetime.datetime.now()
     str1 = now.strftime("%a_%b%d_%H_%M")
     fn = "Apartment_Availability_" + str1 + ".xlsx"
@@ -42,6 +46,9 @@ def build_outfile_name():
 
 
 def create_populate_url_dict():
+    """
+    Create a dict and populate it with name - url pairs for the different Irvine Company Apartment locations.
+    """
     url_dict = {}
     url_dict[
         "The Village"
@@ -65,23 +72,33 @@ def create_populate_url_dict():
 
 
 def create_populate_sheet_headers(name, wb):
+    """
+    Create a new sheet in the workbook with the name of the Apartment complex. Also populates the headers.
+    """
     wb.create_sheet(title=name)
     sheet = wb[name]
-    sheet.cell(row=1, column=1).value = "Identifier"
-    sheet.cell(row=1, column=2).value = "Building No."
-    sheet.cell(row=1, column=3).value = "Building Name"
-
-    sheet.cell(row=1, column=4).value = "Floorplan"
-    sheet.cell(row=1, column=5).value = "Amenity"
-    sheet.cell(row=1, column=6).value = "Beds"
-    sheet.cell(row=1, column=7).value = "Baths"
-    sheet.cell(row=1, column=8).value = "Size (Sq. Ft.)"
-    sheet.cell(row=1, column=9).value = "Price ($)"
-    sheet.cell(row=1, column=10).value = "Term (months)"
-    sheet.cell(row=1, column=11).value = "Date Available"
+    array = [
+        "Identifier",
+        "Building No.",
+        "Building Name",
+        "Floorplan",
+        "Amenity",
+        "Beds",
+        "Baths",
+        "Size (Sq. Ft.)",
+        "Price ($)",
+        "Term (months)",
+        "Date Available",
+    ]
+    for index, val in enumerate(array):
+        sheet.cell(row=1, column=index + 1).value = val
+    return wb[name]
 
 
 def convert_to_int(str1):
+    """
+    Try to convert a string to int if it is numeric, otherwise returns the string itself.
+    """
     try:
         num = int(str1)
         return num
@@ -90,6 +107,9 @@ def convert_to_int(str1):
 
 
 def convert_to_float(str1):
+    """
+    Try to convert a string to float if it is numeric, otherwise returns the string itself.
+    """
     try:
         num = float(str1)
         return num
@@ -97,49 +117,40 @@ def convert_to_float(str1):
         return str1
 
 
-def parse_ica_page(name, url, wb):
-    # url = "https://www.irvinecompanyapartments.com/locations/orange-county/irvine/spectrum/village-at-irvine-spectrum/availability.html"
+def get_page_source(url):
+    """
+    Open browser using webdriver, load the url and get the html source.
+    """
     browser = webdriver.Chrome()
-
     browser.get(url)
-
-    # source = requests.get(
-    #    "https://www.irvinecompanyapartments.com/locations/orange-county/irvine/spectrum/village-at-irvine-spectrum/availability.html#"
-    # ).text
-
     html_source = browser.page_source
     browser.quit()
+    return html_source
 
-    # input = open("source_soup.html", "r")
+
+def parse_ica_page(name, url, wb):
+    """
+    Parses the html source of the availablility page of the apartment complex website and writes all the currently available apartments to an exccel sheet in the workbook provided.
+    """
+    html_source = get_page_source(url)
     soup = BeautifulSoup(html_source, "lxml")
+    # the "results-list loaded" list has all the currently available apartments in an html list.
     all_apartments_list = soup.find_all("ul", class_="results-list loaded")
-    # printer.pprint(soup)
-
-    # soup_output = open("soup_output.txt", "w")
-    # uprint(soup, file=soup_output)
-    # individual_apartments = all_apartments_list.find("li")
-    # print(all_apartments_list[0], file=soup_output)
+    # then we can find all list entries, each entry is an apartment complex.
     individual_apartments = all_apartments_list[0].find_all("li")
-    junk_value = individual_apartments.pop()
-    text = junk_value.get_text()
-    # text.replace(" ", "")
-    # print("Text is *{}*".format(text))
-    # if re.match("\n\nNeed More Options.*", text):
-    #    pass
-    # else:
-    #    exit("The last value is not need more options, please recheck!")
 
+    sheet = create_populate_sheet_headers(name, wb)
     row = 1
     column = 1
-    create_populate_sheet_headers(name, wb)
-    sheet = wb[name]
 
+    # now we iterate over every available apartment, parse the html and write data to excel sheet.
     for apartment in individual_apartments:
         text = apartment.get_text()
         if re.match("\n\nNeed More Options.*", text):
             continue
 
         name = apartment.find("h5").get_text()
+
         floorplan_details = name.split(" - ")
         try:
             identifier, building, floorplan = floorplan_details
@@ -153,6 +164,7 @@ def parse_ica_page(name, url, wb):
         amenity = apartment.find("div", class_="featured-amenity").get_text()
         if not amenity:
             amenity = "None"
+
         divs = apartment.find_all("div")
 
         details = divs[1].get_text()
@@ -181,23 +193,28 @@ def parse_ica_page(name, url, wb):
         term_length = convert_to_int(term_length)
         building_number = convert_to_int(building_number)
 
+        array = [
+            identifier,
+            building_number,
+            building_name,
+            floorplan,
+            amenity,
+            no_of_beds,
+            no_of_baths,
+            size,
+            cost,
+            term_length,
+            availability,
+        ]
         row += 1
-        sheet.cell(row=row, column=1).value = identifier
-        sheet.cell(row=row, column=2).value = building_number
-        sheet.cell(row=row, column=3).value = building_name
+        for index, val in enumerate(array):
+            col = index + 1
+            sheet.cell(row=row, column=index + 1).value = val
 
-        sheet.cell(row=row, column=4).value = floorplan
-
-        sheet.cell(row=row, column=5).value = amenity
-        sheet.cell(row=row, column=6).value = no_of_beds
-        sheet.cell(row=row, column=7).value = no_of_baths
-        sheet.cell(row=row, column=8).value = size
-        sheet.cell(row=row, column=9).value = cost
-        sheet.cell(row=row, column=10).value = term_length
-        sheet.cell(row=row, column=11).value = availability
-
+    # adding filters to headers
     sheet.auto_filter.ref = sheet.dimensions
 
+    # changing cell width for better looking sheets
     dims = {}
     for row in sheet.rows:
         for cell in row:
@@ -207,20 +224,6 @@ def parse_ica_page(name, url, wb):
                 )
     for col, value in dims.items():
         sheet.column_dimensions[col].width = value + 3
-
-        # print(
-        #    "Apartment *{}* Amenity *{}* Bed *{}* Bath *{}* Size *{}* Price *{}* Term *{}* Avail *{}*".format(
-        #        name,
-        #        amenity,
-        #        no_of_beds,
-        #        no_of_baths,
-        #        size,
-        #        cost,
-        #        term_length,
-        #        availability,
-        #    )
-        # )
-        # print("##############################")
 
 
 if __name__ == "__main__":
